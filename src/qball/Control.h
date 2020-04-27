f87c154810014386e3a037195fb4cc039b54972b
Christopher Corey Shepard <chrsshpr@live.unc.edu>
2020-04-27 15:06:48 -0400

    add hf in control, accidentally deleted so re-adding

--- src/qball/Control.h
+++ src/qball/Control.h
@@ -153,11 +153,13 @@ struct Control
   D3vector initial_vector_potential;
   double laser_freq;
   D3vector laser_amp;
-  int saveholefreq,saveelecfreq;
-  int holeindex,elecindex;
-  string saveholefilebase,saveelecfilebase;
+  int saveholefreq,saveelecfreq,saveNOfreq;
+  int holeindex,elecindex,NOindex;
+  string saveholefilebase,saveelecfilebase,saveNOfilebase;
+
   D3vector sine_field;
   bool  compute_sine_field ;
+  double hf;
 
 };
 #endif
