90f6743e27df165123859bec11ad09266ca6920d
Christopher Corey Shepard <chrsshpr@live.unc.edu>
2020-11-16 16:50:38 -0500

    update variable name

--- src/vars/Gaussianfield.h
+++ src/vars/Gaussianfield.h
@@ -12,50 +12,51 @@
 //
 ////////////////////////////////////////////////////////////////////////////////
 //
-// Cosinefield.h
+// Gaussianfield.h
 //
 ////////////////////////////////////////////////////////////////////////////////
 
-#ifndef COSINEFIELD_H
-#define COSINEFIELD_H
+#ifndef GAUSSIANFIELD_H
+#define GAUSSIANFIELD_H
 
 #include<iostream>
 #include<iomanip>
 #include<sstream>
 #include<stdlib.h>
 #include <math/d3vector.h>
+//#include<math.h>
 
 #include <qball/Sample.h>
 
-class Cosinefield : public Var
+class Gaussianfield : public Var
 {
   Sample *s;
 
   public:
 
-  char const*name ( void ) const { return "cosine_field"; };
+  char const*name ( void ) const { return "gaussian_field"; };
 
 ///input should be formatted direction amplitude period
   int set ( int argc, char **argv )
   {
     if ( argc != 4 )
     {
-      ui->error ("cosine_field takes 4 input:direction strength frequency");
+      ui->error ("gaussian_field takes 4 input:direction strength frequency");
       return 1;
     }
 
     if (argv[1] == "X" or argv[1] == "x" )
-	     s->ctrl.cosine_field[0] = 0;
+	     s->ctrl.gaussian_field[0] = 0;
     if (argv[1] == "Y" or argv[1] == "y" )
-             s->ctrl.cosine_field[0] = 1;
+             s->ctrl.gaussian_field[0] = 1;
     if (argv[1] == "Z" or argv[1] == "z" )
-             s->ctrl.cosine_field[0] = 2;
+             s->ctrl.gaussian_field[0] = 2;
 
-    s->ctrl.cosine_field[1] = atof(argv[2]);
-    s->ctrl.cosine_field[2] = atof(argv[3]);
-//    s->ctrl.cosine_field[3] = atof(argv[4]);
-//    s->ctrl.cosine_field[3] = atof(argv[5]);
-    s->ctrl.compute_cosine_field = true;
+    double ev2au = 0.0367493;
+    s->ctrl.gaussian_field[1] = atof(argv[2]);
+    double fwhm = atof(argv[3]);
+    s->ctrl.gaussian_field[2] = fwhm*ev2au; //convert input in ev to au 
+    s->ctrl.compute_gaussian_field = true;
 
     return 0;
   }
@@ -66,20 +67,18 @@ class Cosinefield : public Var
      st.setf(ios::left,ios::adjustfield);
      st << setw(10) << name() << " = ";
      st.setf(ios::right,ios::adjustfield);
-     st << "direction\t"  << s->ctrl.cosine_field[0] << " "
-        << "strength\t"   << s->ctrl.cosine_field[1] << " "
-        << "frequency\t"  << s->ctrl.cosine_field[2] << " ";
-//        << "gamma\t"      << s->ctrl.cosine_field[3] << " ";
-//        << "t0\t"      << s->ctrl.cosine_field[4] << " ";
+     st << "direction\t"  << s->ctrl.gaussian_field[0] << " " 
+        << "strength\t"   << s->ctrl.gaussian_field[1] << " "
+        << "frequency\t"  << s->ctrl.gaussian_field[2] << " ";
      return st.str();
   }
 
-  Cosinefield(Sample *sample) : s(sample)
+  Gaussianfield(Sample *sample) : s(sample)
   {
-    s->ctrl.compute_cosine_field = false;	  
-    s->ctrl.cosine_field[0] = 0.0;
-    s->ctrl.cosine_field[1] = 0.0;
-    s->ctrl.cosine_field[2] = 0.0;
+    s->ctrl.compute_gaussian_field = false;	  
+    s->ctrl.gaussian_field[0] = 0.0;
+    s->ctrl.gaussian_field[1] = 0.0;
+    s->ctrl.gaussian_field[2] = 0.0;
   }
 };
 #endif
